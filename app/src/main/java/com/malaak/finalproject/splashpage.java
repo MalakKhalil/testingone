8e08d89b7bf445dfcd5bee54a24ea3b69b9bf77c
MalaakKhalil <malaak31999@gmail.com>
2021-11-01 04:53:01 -0700

    initial commit

---
+++ app/src/main/java/com/malaak/finalproject/splashpage.java
@@ -0,0 +1,14 @@
+package com.malaak.finalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class splashpage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashpage);
+    }
+}
